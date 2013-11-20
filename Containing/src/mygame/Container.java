6946930c20fd681dd0c708136d319b574ceaebf6
Ruben Vogelvanger <rubie_cool@hotmail.com>
2013-11-20 14:35:43 +0100

    Container + crane

--- Containing/src/mygame/Container.java
+++ Containing/src/mygame/Container.java
@@ -5,79 +5,32 @@
 package mygame;
 
 import com.jme3.math.Vector3f;
-import java.util.Date;
-import javax.xml.bind.annotation.XmlSeeAlso;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 
 /**
  *
- * @author Wessel
+ * @author Ruben
  */
-public class Container {
+public class Container extends Node {
     //Properties
-    String id;                      //Container id
-    Date dateArrival;               //Date of arrival
-    Date dateDeparture;             //Date of scheduled departure
-    int transportTypeArrival;       //Type of container arrival
-    int transportTypeDeparture;     //Type of container departure
-    String cargoCompanyArrival;     //Company handling container arrival
-    String cargoCompanyDeparture;   //Company handling container departure
-    Vector3f position;              //Position on arrival transport
-    Vector3f bufferPosition;        //Position of container inside buffer
-    String owner;                   //Owner of this container
-    int containerNumber;            //Container number
-    Vector3f size;                  //Changed to Vector3f, Dimension halds only two values
-    int weightEmpty;                //Container weight when empty
-    int weightLoaded;               //Container weight when loaded
-    String contents;                //Contents of this container
-    String contentType;             //Type of contents
-    String contentDanger;           //Danger of contents
-    String iso;                     //Container standard
+    public String id;           //Container id
+    private Spatial model;      //Container model
+    public Vector3f position;   //Container position in transport or buffer
+    
     
     //Constructor
     /**
      *
      * @param id
-     * @param dateArrival
-     * @param dateDeparture
-     * @param transportTypeArrival
-     * @param transportTypeDeparture
-     * @param cargoCompanyArrival
-     * @param cargoCompanyDeparture
-     * @param position
-     * @param owner
-     * @param containerNumber
-     * @param size
-     * @param weightEmpty
-     * @param weightLoaded
-     * @param contents
-     * @param contentType
-     * @param contentDanger
-     * @param iso
+     * @param model 
+     * @param position  
      */
-    public Container(String id, Date dateArrival, Date dateDeparture,
-            int transportTypeArrival, int transportTypeDeparture,
-            String cargoCompanyArrival, String cargoCompanyDeparture,
-            Vector3f position, String owner, int containerNumber,
-            Vector3f size, int weightEmpty, int weightLoaded,
-            String contents, String contentType, String contentDanger,
-            String iso){
+    public Container(String id, Spatial model, Vector3f position){
         this.id = id;
-        this.dateArrival = dateArrival;
-        this.dateDeparture = dateDeparture;
-        this.transportTypeArrival = transportTypeArrival;
-        this.transportTypeDeparture = transportTypeDeparture;
-        this.cargoCompanyArrival = cargoCompanyArrival;
-        this.cargoCompanyDeparture = cargoCompanyDeparture;
+        this.model = model;
         this.position = position;
-        //NOTE: bufferPosition remains null until container is assigned to a buffer!
-        this.owner = owner;
-        this.containerNumber = containerNumber;
-        this.size = size;
-        this.weightEmpty = weightEmpty;
-        this.weightLoaded = weightLoaded;
-        this.contents = contents;
-        this.contentType = contentType;
-        this.contentDanger = contentDanger;
-        this.iso = iso;
+        this.attachChild(this.model);
     }
+    
 }
---
+++ Containing/src/mygame/Crane.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
+
+/**
+ *
+ * @author Ruben
+ */
+public abstract class Crane extends Node{
+    /**
+     * Crane ID
+     */
+    public String id;
+    /**
+     * Crane Position
+     */
+    public Vector3f position;
+
+    /**
+     *
+     * @param cont
+     */
+    public abstract void loadContainer(Container cont);
+    /**
+     *
+     * @param cont
+     */
+    public abstract void getContainer(Container cont);
+}

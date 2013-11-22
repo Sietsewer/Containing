9f531eaad9c9bfdaea88e3ad2fec7020b64c3e4b
wsteenhuis <wsteenhuis.1337@gmail.com>
2013-11-22 11:43:02 +0100

    Kleine bugfix
    Had buffer op verkeerde locatie staan omdat Sietse de hele dock had
    verplaatst sinds ik de vorige positie had bepaald.

--- ContainingSimulator/src/mygame/Main.java
+++ ContainingSimulator/src/mygame/Main.java
@@ -91,7 +91,7 @@ public class Main extends SimpleApplication {
             buffers[i] = new Buffer();
             rootNode.attachChild(buffers[i]);
             //some magic number abuse here, snap buffers to proper location on map:
-            buffers[i].setLocalTranslation(111.76f + (i * 22.035f), 11, -485);
+            buffers[i].setLocalTranslation(111.76f + (i * 22.035f), 11, 115);
         }
 
         //Init of the small blue plane, representing water.

738e7d211fdda642b17b8301171c3c236aa15ddd
LenDJ <L45jongh@hotmail.com>
2013-11-22 09:53:41 +0100

    PathNode en PathFinder voor Controller
    finished

--- ContainingController/src/containingcontroller/PathFinder.java
+++ ContainingController/src/containingcontroller/PathFinder.java
@@ -2,51 +2,316 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package containingcontroller;
+package containingpathtest;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
- *
- * @author User
+ *@author Len
+ * Class that creates map of all PathNodes
+ * contains method for calculating shortest Path (Dijkstra algorithm)
  */
 public class PathFinder {
     
-    List<PathNode> map = new ArrayList();
+    private List<PathNode>map = new ArrayList(); //complete map
+    private List<PathNode>mapCSE = new ArrayList();  //seacranes
+    private List<PathNode>mapCBA = new ArrayList(); //bargecranes
+    private List<PathNode>mapCTR = new ArrayList(); //traincranes
+    private List<PathNode>mapCLO = new ArrayList(); // lorrycranes
+    private List<PathNode>mapMR = new ArrayList();  //main points
+    private List<PathNode>mapBA = new ArrayList(); //buffer a
+    private List<PathNode>mapBB = new ArrayList(); //buffer b
     
+    private static String lblBA = "bfa";//buffer a points
+    private static String lblBB = "bfb";//buffer b points
+    private static String lblCTR = "ctr";//cranes train points
+    private static String lblCLO = "clo"; //cranes lorry points
+    private static String lblCSE = "cse";//cranes sea points
+    private static String lblCBA = "cba"; //cranes barge points
+    private static String lblM = "m"; //main points
+    private float smallCost= 0.5f; //smallest cost (the lorry points)
+
     public PathFinder()
     {
-        
     }
     
-    public List<PathNode> getShortestPath(String srce, String dest) throws Exception
+    /**
+     *Creates map in steps
+     */
+    public void createMap()
+    {
+        createBPoints(); //create buffer
+        createCBA(); //create barge
+        createCLO(); //create lorry
+        createCTR(); //create train
+        createCSE(); //create sea
+        connectGreen(); //create mainpoints and if necessary connect to other points
+    }
+    
+    
+    /**
+     *Creates all PathNodes connected to buffers
+     */
+    private void createBPoints()
+    { 
+        float cost = smallCost*2;
+        PathNode previousA = new PathNode(lblBA+1);
+        PathNode previousB = new PathNode(lblBB+1);
+        getMapBA().add(previousA);
+        getMapBB().add(previousB);
+        
+        for(int i = 2;i<=63;i++)
+        {
+         PathNode BAi = new PathNode(lblBA+i);
+         PathNode BBi = new PathNode(lblBB+i);
+         getMapBA().add(BAi);
+         getMapBB().add(BBi);
+         
+         link(previousA,BAi,cost);
+         link(previousB,BBi,cost);
+
+         previousA = BAi;
+         previousB = BBi;
+        }
+        
+        getMap().addAll(getMapBA());
+        getMap().addAll(getMapBB());
+    }
+    
+    /**
+     * Creates all PathNodes of the BargeCranes
+     */
+    private void createCBA()
+    {
+        float cost = smallCost*6;
+        PathNode previousNode = new PathNode(lblCBA+1);
+        int i=2;
+        getMapCBA().add(previousNode);
+        
+        while(i<=8)
+        {
+            PathNode CLi = new PathNode(lblCBA+i);
+            getMapCBA().add(CLi);
+
+            link(CLi,previousNode,cost);
+            previousNode = CLi;
+            i++;
+        }
+        getMap().addAll(getMapCBA());
+    }
+   
+    /**
+     *Inserts a PathNode between two connected PathNodes
+     * @param idA = connection 1
+     * @param idC = connection 2
+     * @param b   = the insertion node
+     * @param costA = cost of connection 1 to insertion node
+     * @param costC = cost of connection 2 to insertion node
+     */
+    private void insertNode(String idA, String idC, PathNode b, float costA, float costC)
+    {
+        PathNode tmpNode = null;
+        for(PathNode nodeA : getMap())
+        {
+            if(nodeA.getId().equals(idA))
+            {
+                for(PathNode cNode : nodeA.getNeighbours())
+                {
+                    if(cNode.getId().equals(idC))
+                    {
+                        tmpNode = cNode;
+                        link(nodeA,b,costA);
+                        link(b,tmpNode,costC);
+                        delink(nodeA,cNode);
+                    }
+                }
+            }
+        }
+    }
+    
+    /**
+     *Creates PathNodes of all SeaCranes
+     */
+    private void createCSE()
+    {
+        float cost = smallCost*5;
+        int i = 2;
+        PathNode previousNode = new PathNode(lblCSE+1);
+        getMapCSE().add(previousNode);
+          
+        while(i<=10)
+        {
+            PathNode cseNode = new PathNode(lblCSE+i);
+            getMapCSE().add(cseNode);
+
+            link(cseNode,previousNode,cost);
+            previousNode = cseNode;
+            i++;
+        }
+        getMap().addAll(getMapCSE());
+    }
+    
+    /**
+     *Creates PathNodes of all TrainCranes
+     */
+    private void createCTR()
+    {
+        float cost = smallCost * 18;
+        int i = 2;
+        PathNode previousNode =  new PathNode(lblCTR+1);
+        getMapCTR().add(previousNode);
+
+       while(i <= 4)
+        {
+            PathNode ctrNode = new PathNode(lblCTR+i);
+            getMapCTR().add(ctrNode);
+            link(ctrNode,previousNode,cost);
+            previousNode = ctrNode;
+            i++;
+        }
+        getMap().addAll(getMapCTR());
+    }
+    
+    /**
+     *Creates PathNodes of all LorryCranes
+     */
+    private void createCLO()
+    {
+        float cost = smallCost;
+        PathNode previousNode = new PathNode(lblCLO+1);
+        int i = 2;
+        getMapCLO().add(previousNode);
+
+        while(i<=20)
+        {
+            PathNode cloNode = new PathNode(lblCLO+i);
+            getMapCLO().add(cloNode);
+            link(cloNode,previousNode,cost);
+            previousNode = cloNode;
+            i++;
+        }
+        getMap().addAll(getMapCLO());
+    }
+    
+    /**
+     *Creates all MainRoad PathNodes and
+     * connects all existing PathNodes to these MainRoad PathNodes
+     */
+    private void connectGreen()
+    {
+        //green boundary points connecting buffer row A
+        PathNode[] greenR = new PathNode[16];
+        for(int i =0; i < greenR.length;i++)
+        {
+            greenR[i] = new PathNode(lblM+(i+1));
+            getMapMR().add(greenR[i]);
+        }
+
+        //connect row BB with green dots
+        link(greenR[0],getMapBB().get(0),smallCost*2);
+        this.insertNode(lblBB+"30", lblBB+"31", greenR[3], smallCost, smallCost);
+        this.insertNode(lblBB+"34", lblBB+"35", greenR[4], smallCost, smallCost);
+        this.insertNode(lblBB+"44", lblBB+"45", greenR[5], smallCost, smallCost);
+        link(greenR[6],getMapBB().get(62),smallCost*6);
+        
+        //connect green dots counter clockwise
+        link(greenR[0],greenR[1],smallCost*3);// s
+        link(greenR[1],getMapCBA().get(0),smallCost*14);//e
+        link(greenR[2],getMapCBA().get(7),smallCost*6);// n
+        link(greenR[2],greenR[3],smallCost*3);
+        link(greenR[4],getMapCLO().get(0),smallCost*2);
+        link(greenR[5],getMapCLO().get(19),smallCost*2);
+        link(greenR[6],greenR[7],smallCost*40);
+        link(greenR[7],greenR[8],smallCost*4);
+        link(greenR[8],greenR[9],smallCost*3);
+        link(greenR[8],getMapBA().get(62),smallCost);
+        link(greenR[9],getMapCTR().get(3),smallCost*30);
+        link(greenR[10],getMapCTR().get(0),smallCost*4);
+        link(greenR[10],greenR[11],smallCost*3);
+        this.insertNode(lblBA+"1",lblBA+"2",greenR[11],smallCost,smallCost);
+        link(greenR[12],getMapBA().get(0),smallCost*2); //
+        link(greenR[12],greenR[15],smallCost*38);
+        link(greenR[12],greenR[13],smallCost*3);
+        link(greenR[13],getMapCSE().get(0),smallCost*4);
+        link(greenR[14],getMapCSE().get(9),smallCost*4);
+        link(greenR[14],greenR[15],smallCost*3);
+        link(greenR[15],greenR[0],smallCost*3);
+
+        getMap().addAll(getMapMR());
+    }
+    
+    /**
+     *Connects two PathNodes to each other
+     * @param a = PathNode 1
+     * @param b = PathNode 2
+     * @param cost = cost of 1 to 2
+     */
+    private void link(PathNode a, PathNode b, float cost)
+    {
+        a.addNeighbour(b, cost);
+        b.addNeighbour(a, cost);
+    }
+    
+    /**
+     *De-connects two PathNodes from each other
+     * @param a = PathNode 1
+     * @param b = PathNode 2
+     */
+    private void delink(PathNode a,PathNode b)
+    {
+        a.removeNeighbour(b);
+        b.removeNeighbour(a);
+    }
+    
+    
+
+    /**
+     * Overload of getShortestPath(String,String,boolean)
+     * @param srce
+     * @param dest
+     * @return
+     * @throws Exception
+     */
+    public List<PathNode> getShortestPath(PathNode srce, PathNode dest) throws Exception
+    {
+        try
+        {
+        return getShortestPath(srce, dest, false);
+        }
+        catch(Exception ex)
+        {
+            throw ex;
+        }
+    }
+    
+        /**
+     * Calculates the shortest Path from a source to a destination by using the Dijkstra Algorithm
+     * Checks each neighbor of a PathNode starting from source 
+     * if neighbor-cost is larger than current-cost, neighbor-cost is current-cost, neighbor-parent is current PathNode
+     * in the end loop back from destination to source by adding all parents to list
+     * the list is the shortest path
+     * @param srce
+     * @param dest
+     * @param optimize = if true-> method-call to optimize() to optimize the path
+     * @return
+     * @throws Exception
+     */
+    public List<PathNode> getShortestPath(PathNode srce, PathNode dest, boolean optimize) throws Exception
     {
        List<PathNode>alreadyChecked = new ArrayList();
        List<PathNode>shortestRoute = new ArrayList();
        List<PathNode>unSettledNodes = new ArrayList();
        
-       PathNode srceNode = null;
-       PathNode destNode = null;
-       
-       for(int i =0; i < map.size();i++)
-       {
-           PathNode curNode = map.get(i);
-           if(curNode.getId().equals(srce))
-           {
-            srceNode = curNode;
-           }
-           else if (curNode.getId().equals(dest))
-           {
-            destNode = curNode;   
-           }
-       }
+       PathNode srceNode = srce;
+       PathNode destNode = dest;
        
        if(srceNode !=null && destNode != null)
        {
        srceNode.setCost(0, null); //start cost}
        unSettledNodes.add(srceNode);
-       
+       float totalCost = 1000;
        while(unSettledNodes.size()>0)
        {
            PathNode node =  getMinimum(unSettledNodes);
@@ -55,30 +320,92 @@ public class PathFinder {
            
           for(PathNode neighbour : node.getNeighbours())
           {
-              if(alreadyChecked.contains(neighbour))
+              float curCost = node.getCost() + node.getCostNeighb(neighbour);
+              if(alreadyChecked.contains(neighbour)||curCost > totalCost)
               {
                   continue;
               }
-              if(neighbour.getCost()==-1||neighbour.getCost() > (node.getCost() + node.getCostNeighb(neighbour)))
+             
+              if(neighbour.getCost()==-1||neighbour.getCost() > curCost)
               {
-                  neighbour.setCost((node.getCost() + node.getCostNeighb(neighbour)),node);
+                  neighbour.setCost(curCost,node);
+              }
+              if(neighbour.equals(destNode))
+              {
+                  if(neighbour.getCost()< curCost)
+                  {
+                      totalCost = curCost;
+                  }
               }
               unSettledNodes.add(neighbour);
        }
       }     
-       shortestRoute.add(destNode);
-       destNode.getRoute(shortestRoute);
-
+       shortestRoute.add(destNode); //add dest to shortest path
+       destNode.getRoute(shortestRoute); //create shortest path
+       Collections.reverse(shortestRoute); //reverse path , srce to dest
+       if(optimize)
+       {
+       optimize(shortestRoute); //delete useless nodes
+       }
        return shortestRoute;
     }
        else
        {
-           throw new Exception("source and destination are not connected!");
+           throw new Exception("source and/or destination not found in map");
        }
        
     }
     
-     public PathNode getMinimum(List<PathNode> neighbours)
+    
+    /**
+     *
+     * @param id
+     * @return
+     */
+    private PathNode getPathNode(String id)
+    {
+        for(int i =0; i < getMap().size();i++)
+       {
+           PathNode curNode = getMap().get(i);
+           if(curNode.getId().equals(id))
+           { 
+           return curNode;
+           }
+       }
+        return null;
+    }
+    
+    /**
+     * optimizes the path by deleting nodes on a linear path 
+     * east-east-east  will be optimized as east-east, east-north-east cant be optimized.
+     * @param shortestRoute
+     */
+    public void optimize(List<PathNode>shortestRoute)
+    {
+        for(int i =1; i < shortestRoute.size()-1;i++)
+        {
+         
+         String prevID = shortestRoute.get(i-1).getId();
+         String curID = shortestRoute.get(i).getId();
+         String nextID = shortestRoute.get(i+1).getId();
+         
+         if(prevID.length()>2&&curID.length()>2&&nextID.length()>2)
+         {
+         if(curID.substring(0, 3).equals(nextID.substring(0, 3)) && curID.substring(0, 3).equals(prevID.substring(0, 3)))
+         {
+             shortestRoute.remove(i);
+             i--;
+         }
+         }
+        }
+    }
+    
+     /**
+     * returns PathNode with lowest cost
+     * @param neighbours
+     * @return
+     */
+    public PathNode getMinimum(List<PathNode> neighbours)
      {
      PathNode minnode = null;
      float mindistance = 9999f;
@@ -92,5 +419,64 @@ public class PathFinder {
       }
       return minnode;
      }
+
+    /**
+     * @return the complete map
+     */
+    public List<PathNode> getMap() {
+        return map;
+    }
+
+    /**
+     * @return the mapCBA
+     */
+    public List<PathNode> getMapCBA() {
+        return mapCBA;
+    }
+
+    /**
+     * @return the mapCTR
+     */
+    public List<PathNode> getMapCTR() {
+        return mapCTR;
+    }
+
+    /**
+     * @return the mapCLO
+     */
+    public List<PathNode> getMapCLO() {
+        return mapCLO;
+    }
+
+    /**
+     * @return the mapMR
+     */
+    public List<PathNode> getMapMR() {
+        return mapMR;
+    }
+
+    /**
+     * @return the mapBA
+     */
+    public List<PathNode> getMapBA() {
+        return mapBA;
+    }
+
+    /**
+     * @return the mapBB
+     */
+    public List<PathNode> getMapBB() {
+        return mapBB;
+    }
+
+    /**
+     * @return the mapCSE
+     */
+    public List<PathNode> getMapCSE() {
+        return mapCSE;
+    }
     
 }
+
+    
+
--- ContainingController/src/containingcontroller/PathNode.java
+++ ContainingController/src/containingcontroller/PathNode.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package containingcontroller;
+package containingpathtest;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,42 +10,87 @@ import java.util.List;
 
 /**
  *
- * @author User
+ * @author Len
  */
 public class PathNode {
     
+    /**
+     * name of PathNode
+     */
     private String id;
+    /**
+     *cost of this PathNode
+     */
     private float cost = -1f;
+    /**
+     *the PathNode that is responsible for setting this cost
+     */
     private PathNode parent;
+    /**
+     * Contains the linked PathNodes to this one
+     */
     private List<PathNode>neighbours;
     
-    private HashMap<PathNode,Float>neighb; //neighbor with cost to this node
+    /**
+     * Contains linked PathNodes with the costs to this one
+     */
+    private HashMap<PathNode,Float>neighb = new HashMap(); //neighbor with cost to this node
     
+    /**
+     * Constructor
+     * @param id
+     */
     public PathNode(String id)
     {
         this.id = id;
         neighbours = new ArrayList();
     }
-    
-    public String getParentId()
+    /**
+     * print this node with its neighbors id and costs
+     */
+    public void printWithNeighbours()
     {
-        if(parent!= null)
-        {
-            return parent.id;
-        }
-        return " ";
+        System.out.print(this.id + "  ");
+                for(PathNode node : neighbours)
+                {
+                    System.out.print(node.getId() + " - "+ this.getCostNeighb(node)+ "      ");
+                }
+                System.out.println();
     }
+    /**
+     * returns ID
+     * @return
+     */
     public String getId()
     {
         return this.id;
     }
     
+    /**
+     * set the shortest cost for now coming from parent
+     * @param cost
+     * @param parent
+     */
     public void setCost(float cost, PathNode parent)
     {
         this.cost = cost; 
         this.parent = parent;
     }
+        
+    /**
+     *returns cost
+     * @return
+     */
+    public float getCost()
+    {
+     return this.cost;   
+    }
     
+    /**
+     * add all parents to the list
+     * these are all the nodes that create the shortest path
+     * @param route
+     */
     public void getRoute(List<PathNode>route)
     {
        if(parent!=null)
@@ -54,6 +99,11 @@ public class PathNode {
            parent.getRoute(route);
        }
     }
+    /**
+     * returns cost of this PathNode to a neighbor
+     * @param neighbour
+     * @return
+     */
     public float getCostNeighb(PathNode neighbour)
     {
         if(neighb.size()>0)
@@ -65,12 +115,13 @@ public class PathNode {
             return 1000;
         }
     }
+
     
-    public float getCost()
-    {
-     return this.cost;   
-    }
-    
+    /**
+     * add new Neighbor
+     * @param neighbour
+     * @param cost
+     */
     public void addNeighbour(PathNode neighbour, float cost)
     {
         if(!neighbours.contains(neighbour))
@@ -79,6 +130,21 @@ public class PathNode {
             neighb.put(neighbour, cost);
         }
     }
+    
+    /**
+     * remove a neighbor
+     * @param neighbour
+     */
+    public void removeNeighbour(PathNode neighbour)
+    {
+        neighbours.remove(neighbour);
+        neighb.remove(neighbour);
+    }
+    
+    /**
+     *return a list of all neighbors
+     * @return
+     */
     public List<PathNode> getNeighbours()
     {
         return this.neighbours;
@@ -86,3 +152,5 @@ public class PathNode {
 
     
 }
+
+   

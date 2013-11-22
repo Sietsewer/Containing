b38ee7c9febeb16378b767deb8be6612fe968340
Ruben Vogelvanger <rubie_cool@hotmail.com>
2013-11-22 09:45:28 +0100

    Buffer omgezet naar hashmap

--- ContainingController/src/containingcontroller/Buffer.java
+++ ContainingController/src/containingcontroller/Buffer.java
@@ -7,12 +7,14 @@ package containingcontroller;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 /**
  *
  * @author Ruben
  */
 public class Buffer {
+
     /**
      * Id of buffer
      */
@@ -20,7 +22,7 @@ public class Buffer {
     /**
      * Array of all containers
      */
-    public Container[][][] containers;
+    public HashMap<CustomVector3f, Container> containers;
     /**
      * List of all owned AGVs
      */
@@ -32,26 +34,27 @@ public class Buffer {
     /**
      * Hashmap for reserved spaces
      */
-    public HashMap<String,CustomVector3f> reservedSpace;
+    public HashMap<String, CustomVector3f> reservedSpace;
     /**
      * Position of buffer
      */
     public CustomVector3f position;
     public PathNode pathNodeUp;
     public PathNode pathNodeDown;
-
+    private final static CustomVector3f size = new CustomVector3f(6,6,26);
     /**
      * Constructor
+     *
      * @param position
      * @param containers
      * @param ownedAGV
      * @param cranes
      */
     public Buffer(CustomVector3f position, ArrayList<Container> containers, ArrayList<AGV> ownedAGV, ArrayList<Crane> cranes) {
-        this.containers = new Container[6][6][26];
+        this.containers = new HashMap<>(936);
         this.ownedAGV = new ArrayList<>(ownedAGV);
         this.cranes = new ArrayList<>(cranes);
-        this.reservedSpace = new HashMap<>();
+        this.reservedSpace = new HashMap<>(936);
         this.position = position;
     }
 
@@ -62,15 +65,15 @@ public class Buffer {
      */
     public ArrayList<Container> checkDepartingContainers(Date date) {
         ArrayList<Container> departingContainers = new ArrayList<>();
-        for (Container[][] containerArray3 : containers) {
-            for (Container[] containerArray2 : containerArray3) {
-                for (Container container : containerArray2) {
-                    if (container.getDateDeparture().after(date) || container.getDateDeparture().equals(date)) {
-                        departingContainers.add(container);
-                    }
-                }
+
+        for (Entry<CustomVector3f, Container> entry : containers.entrySet()) {
+            Container container = entry.getValue();
+
+            if (container.getDateDeparture().after(date) || container.getDateDeparture().equals(date)) {
+                departingContainers.add(container);
             }
         }
+
         return departingContainers;
     }
 
@@ -81,14 +84,14 @@ public class Buffer {
      * @return Vector3f
      */
     public CustomVector3f findBestBufferPlace(Container container) {
-        for (int z = 0; z < 26; z++) {
-            for (int x = 0; x < 6; x++) {
-                for (int y = 0; y < 6; y++) {
-                    if (containers[x][y][z] != null) {
+        for (int z = 0; z < size.z; z++) {
+            for (int x = 0; size.x < 6; x++) {
+                for (int y = 0; size.y < 6; y++) {
+                    if (containers.get(new CustomVector3f(x, y, z)) != null) {
                         continue;
                     }
                     if (y > 0) {
-                        if (containers[x][y - 1][z].getDateDeparture().after(container.getDateDeparture())) {
+                        if (containers.get(new CustomVector3f(x, y -1, z)).getDateDeparture().after(container.getDateDeparture())) {
                             return new CustomVector3f(x, y, z);
                         } else {
                             continue;
@@ -108,17 +111,17 @@ public class Buffer {
      * @param container
      */
     public void addContainer(Container container, CustomVector3f position) {
-        containers[(int)position.x][(int)position.y][(int)position.z] = container;
+        containers.put(position, container);
     }
-    
+
     public void removeContainer(Container container) {
-        for (int z = 0; z < 26; z++) {
-            for (int x = 0; x < 6; x++) {
-                for (int y = 0; y < 6; y++) {
-                    if(containers[x][y][z].getId().equals(container.getId())) {
-                        containers[x][y][z] = null;
-                    }
-                }
+        for (Entry<CustomVector3f, Container> entry : containers.entrySet()) {
+            CustomVector3f key = entry.getKey();
+            Container mapContainer = entry.getValue();
+
+            if (container == mapContainer) {
+                containers.remove(key);
+                break;
             }
         }
     }

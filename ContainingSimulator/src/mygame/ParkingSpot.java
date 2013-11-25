c6eedb500ac7cc83ce5ef5cdcc68860b9e0cd165
wsteenhuis <wsteenhuis.1337@gmail.com>
2013-11-25 10:00:20 +0100

    properties ParkingSpot gefixt

--- ContainingSimulator/src/mygame/ParkingSpot.java
+++ ContainingSimulator/src/mygame/ParkingSpot.java
@@ -9,5 +9,17 @@ package mygame;
  * @author Sietse
  */
 public class ParkingSpot {
+    public Vector3f translation;        //local translation of parking space
+    public float rotation;              //rotation of parking space
     
+    /**
+     * Constructor
+     * @param translation
+     * @param rotation 
+     */
+    public ParkingSpot(Vector3f translation, float rotation){
+        this.translation = translation;
+        this.rotation = rotation;
+    }
+
 }

ca3e21791e6934285d8b3c2fc539184100a09b22
Sietsewer <sietsewer@gmail.com>
2013-11-25 11:59:22 +0100

    Fixed seacrane model, added lorryPlatform code

--- ContainingSimulator/src/mygame/LorryPlatform.java
+++ ContainingSimulator/src/mygame/LorryPlatform.java
@@ -4,10 +4,51 @@
  */
 package mygame;
 
+import com.jme3.scene.Node;
+
 /**
  *
  * @author Sietse
  */
-public class LorryPlatform {
+public class LorryPlatform extends Node{
     
+    private Container container;
+    private String agvID;
+    private AGV agv;
+    
+    public LorryPlatform(){
+        
+    }
+    
+    public void lorryArrive(){
+        
+    }
+    
+    public void lorryDepart(){
+        
+    }
+    
+    public void agvArrived(AGV arrivedAGV){
+        
+    }
+    
+    public void agvDepard(){
+        
+    }
+    
+    public void fromLorry(){
+        
+    }
+    
+    public void toLorry(){
+        
+    }
+    
+    public void fromAGV(){
+        
+    }
+    
+    public void toAGV(){
+        
+    }
 }

9437af92284f729d8637a6a5486d5017b9786b09
Ruben Vogelvanger <rubie_cool@hotmail.com>
2013-11-27 10:37:41 +0100

    Containing xml nieuw

---
+++ ContainingSimulator/src/containing/xml/SimContainer.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package containing.xml;
+
+
+import org.simpleframework.xml.*;
+
+/**
+ *
+ * @author Hendrik
+ */
+@Root
+public class SimContainer {
+
+    @Attribute
+    private String id;
+    @Element
+    private CustomVector3f indexPosition;
+
+    /**
+     *
+     */
+    public SimContainer() {
+    }
+
+
+    /**
+     *
+     * @return
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @param id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public CustomVector3f getIndexPosition() {
+        return indexPosition;
+    }
+
+    /**
+     *
+     * @param indexPosition
+     */
+    public void setIndexPosition(CustomVector3f indexPosition) {
+        this.indexPosition = indexPosition;
+    }
+}
